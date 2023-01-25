315e91edd9a4f74163f598ad530919c4a168f7e9
Olga <chi89@yandex.ru>
2023-01-25 22:35:20 +0300

    Определяет, является ли пятизначное число палиндромом

---
+++ Task19/Program.cs
@@ -0,0 +1,42 @@
+﻿/*  Напишите программу, которая принимает на вход пятизначное число и проверяет, является ли оно палиндромом.
+
+14212 -> нет
+12821 -> да
+23432 -> да   */
+
+int GetNumber(string message)
+{
+int resultNumber = 0;
+
+while(true)
+{
+Console.WriteLine(message);
+
+if(int.TryParse(Console.ReadLine(), out resultNumber) && 9999 < resultNumber && resultNumber < 100000)
+{
+break;
+}
+else
+{
+Console.WriteLine("Ввели не корректное число. Повторите ввод!\n");
+}
+}
+return resultNumber;
+}
+
+int number = GetNumber("Введите пятизначное число");
+int firstDiditNumber = number / 10000;
+int secondDiditNumber = number / 1000;
+int secondDiditNumber2 = secondDiditNumber % 10;
+int fourthDiditNumber = number / 10;
+int fourthDiditNumber2 = fourthDiditNumber % 10;
+int fifthDiditNumber2 = number % 10;
+
+if (firstDiditNumber == fifthDiditNumber2 && secondDiditNumber2 == fourthDiditNumber2)
+{
+    Console.WriteLine($"Число {number} является палиндромом");
+}
+else
+{
+    Console.WriteLine($"Число {number} не является палиндромом");
+}
